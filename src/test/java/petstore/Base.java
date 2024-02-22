5df6312b1651687089b6fdf462af69586be75f7d
Marcos Riveros <marcosriverosk@gmail.com>
2024-02-21 22:55:04 -0300

    added pom system variables

---
+++ src/test/java/petstore/Base.java
@@ -0,0 +1,15 @@
+package petstore;
+
+import io.restassured.RestAssured;
+import net.thucydides.model.util.EnvironmentVariables;
+
+
+public class Base {
+
+    EnvironmentVariables env;
+
+    public void setup() {
+        RestAssured.baseURI = env.getProperty("restapi.baseurl");
+
+    }
+}
